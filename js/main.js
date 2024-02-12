d9018f4af31bf729b96d5a4badad70acf183a763
Catalina Mar <ma37bs@gmail.com>
2024-02-12 09:07:12 -0300

    subiendo archivos

---
+++ js/main.js
@@ -0,0 +1,65 @@
+//variables
+
+//NOMBRE USUARIO//
+/*let usuario = prompt("Ingresa tu nombre")
+
+//CODIGO POSTAL//
+let codigoPostal= prompt("Ingresa tu codigo postal")*/
+
+//PRECIO DE PRODUCTOS//
+/*const item2 = 9000
+const item3 = 3500
+//TOTAL DEL CARRITO//
+let total = item2 + item3*/
+
+//CONSULTA STOCK DE PRODUCTOS//
+/*let cantidadTarta = prompt("Ingresa la cantidad")*/
+
+//BUCLE CONSULTA COMPRA//
+/*let seguirComprando = true;*/
+
+
+
+//PROCESO: VALIDAR ZONA DE ENTREGA CON CODIGO POSTAL//
+/*console.log( "Hola " + usuario + " vamos a necesitar validar tu zona de entrega");
+console.log(codigoPostal);
+if (codigoPostal == 1602) {
+    alert("Zona de entrega valida");
+} else if (codigoPostal == 1636) {
+    alert("Zona de entrega valida");
+}else if(codigoPostal == 1638){
+    alert("Zona de entrega valida");
+}else if(codigoPostal == 1640){
+    alert("Zona de entrega valida");
+}else{
+    alert("Zona de entrega no valida");
+}
+console.log("Fin del proceso");*/
+
+
+
+//FUNCTION: SUMA DE PRODUCTOS//
+/*function sumar(item2, item3) {
+    return item2 + item3;
+}
+console.log("El total es:" + total);*/
+
+
+//CONSULTA STOCK DE PRODUCTOS//
+/*if (cantidadTarta < 10) {
+    console.log("Tenemos stock");
+} else {
+    console.log("Lo sentimos no tenemos esa cantidad");
+}*/
+
+
+//BUCLE DE COLSULTA SOBRE CONTINUAR CON LA COMPRA//
+/*while (seguirComprando) {
+const opcion = prompt("¿Desea seguir comprando? (Sí/No)");
+    if (opcion.toLowerCase() === "no") {
+    seguirComprando = false;
+    } else {
+    console.log("Quiero seguir mirando")
+    }
+}
+console.log("Esperamos que te guste nuestros productos ¡Gracias por visitarnos!");*/
